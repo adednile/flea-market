892bb74cf05eb3512d8bf499da25beb4441dd3ed
PotterIle <elindeda1@gmail.com>
2025-07-13 22:27:26 +0300

    changes to the shop

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -56,78 +56,86 @@ function Shop(){
         setUserEmail("");
         navigate('/LandingPage');
     };
-const [vendorProducts, setVendorProducts] = useState([]);
 
-useEffect(() => {
-  const stored = localStorage.getItem("products");
-  setVendorProducts(stored ? JSON.parse(stored) : []);
-}, []);
+    //sample product data
+    //ADD MOCK DATA FROM HERE
+    const products = [
+        {
+            name: "Macbook pro",
+            image: require("../assets/macbookpro.jpg"),
+            price: "KES 250,000",
+            category: "Electronics"
+        },
+        {
+            name: "Shoes",
+            image: require("../assets/shoes1-0.jpg"),
+            price: "KES 2,000",
+            category: "Clothing"
+        },
+        {
+          name: "Tumaini dishes",
+          image: require("../assets/tumaini-dishes.jpg"),
+          price: "KES 3,500",
+          category: "Food"
+        },
+        {
+          name: "Siwaka Clothes",
+          image: require("../assets/maasai-wear.jpg"),
+          price: "KES 2,540",
+          category: "Clothing"
 
+        },
+        {
+          name: "Imani Cleaners",
+          image: require("../assets/house-cleaning.jpg"),
+          price: "KES 4,500",
+          category: "Services"
+        },
+        {
+          name: "JBL headphones",
+          image: require("../assets/jbl.jpg"),
+          price: "KES 17,540",
+          category: "Electronics"
 
-const mockProducts = [
-    {
-        name: "Laptop",
-        image: require("../assets/tech-gadgets.png"),
-        price: "KES 30,000",
-        category: "Electronics"
-    },
-    {
-        name: "Shoes",
-        image: require("../assets/shoes1-0.jpg"),
-        price: "KES 2,000",
-        category: "Clothing"
-    },
-    {
-        name: "Tumaini dishes",
-        image: require("../assets/tumaini-dishes.jpg"),
-        price: "KES 3,500",
-        category: "Food"
-    },
-    {
-        name: "Siwaka Clothes",
-        image: require("../assets/maasai-wear.jpg"),
-        price: "KES 2,540",
-        category: "Clothing"
-    },
-    {
-        name: "Imani Cleaners",
-        image: require("../assets/house-cleaning.jpg"),
-        price: "KES 4,500",
-        category: "Services"
-    },
-    {
-        name: "Madaa Pizza",
-        image: require("../assets/pizza-kenya.jpg"),
-        price: "KES 2,450",
-        category: "Food"
-    },
-    {
-        name: "Afro-Dishes",
-        image: require("../assets/githeri.jpg"),
-        price: "KES 500",
-        category: "Food"
-    },
-    {
-        name: "Campus Coffee",
-        image: require("../assets/coffee-image.jpg"),
-        price: "KES 750",
-        category: "Food"
-    },
-    {
-        name: "Chap Chap Tailoring",
-        image: require("../assets/tailoring-services.jpeg"),
-        price: "KES 5,000",
-        category: "Services"
-    },
-    {
-        name: "Kiliwomen Bags",
-        image: require("../assets/women-bags.jpeg"),
-        price: "KES 4,560",
-        category: "Clothing"
-    }
-].map((product, index) => ({ ...product, id: index + 1000 }));
+        },
+        {
+          name: "Ipad pro",
+          image: require("../assets/ipad-pro.jpg"),
+          price: "KES 232,540",
+          category: "Electronics"
 
-const products = [...vendorProducts, ...mockProducts];
+        },
+        {
+          name: "Madaa Pizza",
+          image: require("../assets/pizza-kenya.jpg"),
+          price: "KES 2,450",
+          category: "Food"
+        },
+        {
+          name: "Afro-Dishes",
+          image: require("../assets/githeri.jpg"),
+          price: "KES 500",
+          category: "Food"
+        },
+        {
+          name: "Campus Coffee",
+          image: require("../assets/coffee-image.jpg"),
+          price: "KES 750",
+          category: "Food"
+        },
+        {
+          name: "Chap Chap Tailoring",
+          image: require("../assets/tailoring-services.jpeg"),
+          price: "KES 5,000",
+          category: "Services"
+        },
+        {
+          name: "Kiliwomen Bags",
+          image: require("../assets/women-bags.jpeg"),
+          price: "KES 4,560",
+          category: "Clothing"
+        }
+    ].map((product, index) => ({...product, id: index + 1}));
 
     const addToCart = (product) => {
         const updatedCart = [...cartItems];
